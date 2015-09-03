a8e6023e83dfadee87045e624ea9e4c3875585cd
superjerk <tungsten33@gmail.com>
2015-09-03 14:44:47 -0500

    Update autotrimp.js

--- autotrimp.js
+++ autotrimp.js
@@ -1,2 +1,9 @@
-var temp = game.resources.wood.owned / game.resources.wood.max;
-alert(temp);
+var myVar=setInterval(function () {myTimer()}, 3000);
+
+function myTimer() {
+  var temp = game.resources.wood.owned / game.resources.wood.max;
+    if (temp < .5) {
+      alert(temp);
+    }
+}
+
