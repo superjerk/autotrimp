00fcb2e9186efddf7135f1d56513771484fb0ae6
superjerk <tungsten33@gmail.com>
2015-09-03 15:12:29 -0500

    Update autotrimp.js

--- autotrimp.js
+++ autotrimp.js
@@ -1,9 +1,16 @@
-var myVar=setInterval(function () {myTimer()}, 3000);
+document.getElementById("food").appendChild(document.createTextNode("*"));
+document.getElementById("wood").appendChild(document.createTextNode("*"));
+document.getElementById("metal").appendChild(document.createTextNode("*"));
+var myVar=setInterval(function () {myTimer()}, 5000);
 
 function myTimer() {
-  var temp = game.resources.wood.owned / game.resources.wood.max;
-    if (temp < .5) {
-      alert(temp);
-    }
+  var food = game.resources.food.owned / game.resources.food.max;
+  var wood = game.resources.wood.owned / game.resources.wood.max;
+  var metal = game.resources.metal.owned / game.resources.metal.max;
+  alert(food);
+  alert(wood);
+  alert(metal);
+
+  clearInterval(myVar);
 }
 
