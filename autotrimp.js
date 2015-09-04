66e03899d7be4c033bf2d35de22fc9c5a4ada8c9
superjerk <tungsten33@gmail.com>
2015-09-04 16:18:55 -0500

    Update autotrimp.js

--- autotrimp.js
+++ autotrimp.js
@@ -8,6 +8,8 @@ function myTimer() {
   var wood = game.resources.wood.owned / game.resources.wood.max;
   var metal = game.resources.metal.owned / game.resources.metal.max;
 
+//Buy resource buildings
+
   if (food > .8) {
     buyBuilding('Barn');
   }
@@ -17,6 +19,23 @@ function myTimer() {
   if (metal > .8) {
     buyBuilding('Forge');
   }
+
+//Buy speed upgrades
+
+  if (game.upgrades.Speedfarming.allowed > game.upgrades.Speedfarming.done) {
+     buyUpgrade('Speedfarming');
+  }
+  if (game.upgrades.Speedlumber.allowed > game.upgrades.Speedlumber.done) {
+     buyUpgrade('Speedlumber');
+  }
+  if (game.upgrades.Speedminer.allowed > game.upgrades.Speedminer.done) {
+     buyUpgrade('Speedminer');
+  }
+  if (game.upgrades.Speedscience.allowed > game.upgrades.Speedscience.done) {
+   buyUpgrade('Speedscience');
+  }
+
+//Buy coordination
 
   if (game.upgrades.Coordination.allowed > game.upgrades.Coordination.done) {
     if ((game.resources.trimps.realMax() > (game.resources.trimps.maxSoldiers * 3))) {
