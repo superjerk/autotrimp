1b86b69e8df2ca6b5402f9468d3e75effc42595b
superjerk <tungsten33@gmail.com>
2015-09-15 16:21:02 -0500

    Update autotrimp.js

--- autotrimp.js
+++ autotrimp.js
@@ -4,9 +4,11 @@
 //fix paddings for id=buyCol and id=rightCol to .3%
 document.getElementById("buyCol").style.paddingRight = ".3%";
 document.getElementById("rightCol").style.paddingLeft = ".3%";
+
 //setup talk button
 document.getElementById("buildingsQueue").innerHTML = "<div style='width: 75%; float: left;'><span style='display: block;' id='noQueue'>Nothing in queue...</span><div id='queueItemsHere'></div></div><div style='color: rgb(255, 255, 255); font-size: 1.2em; text-align: center; padding-left: 23px; padding-top: 2px; padding-right: 0px;' class='col-xs-3'><div id='buildingsCollectBtn' class='workBtn pointer noselect' onClick='talk()' style='background: rgb(0, 0, 0) none repeat scroll 0% 0%;'>Talk</div></div>"
 
+//call loop
 var myVar=setInterval(function () {myTimer()}, 10000);
 
 function talk() {
@@ -31,14 +33,14 @@ function myTimer() {
   }
 
 //Buy speed upgrades
-autotrimpupgrades = ["Egg", "UberHut", "UberHouse", "UberMansion", "UberHotel", "UberResort", "Bounty", "Efficiency", "TrainTacular", "Gymystic", "Speedfarming", "Speedlumber", "Speedminer", "Speedscience", "Potency"]
-for (var key in game.upgrades) {
-  if (autotrimpupgrades.indexOf(key) != -1) { 
-    if (game.upgrades[key].allowed > game.upgrades[key].done) {
-      buyUpgrade(key);
+  autotrimpupgrades = ["Egg", "UberHut", "UberHouse", "UberMansion", "UberHotel", "UberResort", "Bounty", "Efficiency", "TrainTacular", "Gymystic", "Speedfarming", "Speedlumber", "Speedminer", "Speedscience", "Potency"]
+  for (var key in game.upgrades) {
+    if (autotrimpupgrades.indexOf(key) != -1) { 
+      if (game.upgrades[key].allowed > game.upgrades[key].done) {
+        buyUpgrade(key);
+      }
     }
   }
-}
 
 //Buy coordination
 
@@ -48,5 +50,6 @@ for (var key in game.upgrades) {
     }
   }
   //clearInterval(myVar);
-}
+}//end loop
+
 
