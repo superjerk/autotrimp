569afb03106705871a80e1b60057460196bb31ae
superjerk <tungsten33@gmail.com>
2015-09-16 14:49:21 -0500

    Update autotrimp.js

--- autotrimp.js
+++ autotrimp.js
@@ -26,9 +26,9 @@ function updateConvo (place) {
   document.getElementById("q").innerHTML = conversation[place].Q;
   document.getElementById("1").innerHTML = conversation[place].R1;
   //document.getElementById("1").onclick = updateConvo(conversation[place].L1);
-  document.getElementById("2").innerHTML = conversation[place].R2;
+  if (conversation[0].R2 !== null) {document.getElementById("2").innerHTML = conversation[place].R2;}
   //document.getElementById("2").onclick = updateConvo(conversation[place].L2);
-  document.getElementById("3").innerHTML = conversation[place].R3;
+  if (conversation[0].R3 !== null) {document.getElementById("3").innerHTML = conversation[place].R3;}
   //document.getElementById("3").onclick = updateConvo(conversation[place].L3);
 }
 
