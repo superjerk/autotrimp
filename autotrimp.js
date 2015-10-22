830eea91dae6e76af0496ba3174bf217a784df6b
FirenX <merz.thilo@gmx.de>
2015-10-22 23:01:41 +0200

    Include Coordinate when buying Coordination

--- autotrimp.js
+++ autotrimp.js
@@ -360,7 +360,7 @@ if (autoTSettings.autoupgrades.enabled == 1) {
 //Buy coordination
 
   if (game.upgrades.Coordination.allowed > game.upgrades.Coordination.done) {
-    if ((game.resources.trimps.realMax() > (game.resources.trimps.maxSoldiers * 3))) {
+    if canAffordCoordinationTrimps() {
       buyUpgrade('Coordination');
       message("We read Coordination together before bedtime, it was sweet. Now let's go kill something.", "Loot", "*eye2", "exotic")
     }
